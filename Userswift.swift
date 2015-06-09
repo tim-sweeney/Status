c3f21213224c512b419fae48bc25f9710d1417f4
tim sweeney <tim_sweeney@me.com>
2015-06-08 19:12:38 -0700

    add models

---
+++ Userswift.swift
@@ -0,0 +1,17 @@
+//
+//  Userswift.swift
+//  Status
+//
+//  Created by Tim Sweeney on 6/8/15.
+//  Copyright (c) 2015 Tim Sweeney. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    var name:String?
+    var bio:String?
+    var username:String?
+    var link:String?
+    var city:String?
+}
---
+++ update.swift
@@ -0,0 +1,16 @@
+//
+//  update.swift
+//  Status
+//
+//  Created by Tim Sweeney on 6/8/15.
+//  Copyright (c) 2015 Tim Sweeney. All rights reserved.
+//
+
+import Foundation
+
+class update {
+    var user: User?
+    var text: String?
+    var date: NSDate?
+    
+}
